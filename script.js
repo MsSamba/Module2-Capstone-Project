46fb8a9bf4be800207ee9b5431127bb27297fb67
MsSamba <sambamichelle1@gmail.com>
2025-04-23 19:20:46 +0300

    Sign Up and Log In Implementation

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+ let loginmodal = document.getElementById("show-login");
+
+ function openLoginModal() {
+    loginmodal.classList.add("login-modal");
+ }
+ 
+ // Auth Handling
+ document.getElementById('show-login').addEventListener('click', () => {
+    document.getElementById('signup-modal').classList.add('hide');
+    document.getElementById('login-modal').classList.remove('hide');
+  });
+  
+  // Signup Handler
+  document.getElementById('signup-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const userData = {
+      name: document.getElementById('fullname').value,
+      email: document.getElementById('email').value,
+      password: document.getElementById('password').value
+    };
+  
+    if (document.getElementById('password').value !== document.getElementById('confirm-password').value) {
+      alert('Passwords do not match!');
+      return;
+    }
+  
+    localStorage.setItem('user', JSON.stringify(userData));
+    alert('Registration successful! Please login.');
+
+    // Check if User exists
+
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+    const enteredEmail = document.getElementById('login-email').value;
+
+    if (storedUser.email === enteredEmail) {
+        alert('User Already Exists! Please Log In');         
+      }
+
+  });
+
+
+
+  
+  // Login Handler
+  document.getElementById('login-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+    const enteredEmail = document.getElementById('login-email').value;
+    const enteredPassword = document.getElementById('login-password').value;
+  
+    if (storedUser && storedUser.email === enteredEmail && storedUser.password === enteredPassword) {
+      alert('Login successful!');
+            
+    } else {
+      alert('Invalid credentials!');
+    }
+  });
+
+//   Budget App Logic
 class BudgetTracker {
     constructor() {
         this.transactions = JSON.parse(localStorage.getItem('transactions')) || [];
